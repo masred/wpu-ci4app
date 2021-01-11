1d8f885a176b4f32e597226174c8744a4fd5b73d
Dwi Fitriyanto <dwifitriyanto123@gmail.com>
2021-01-11 20:13:53 +0700

    add contact function with alamat

--- app/Controllers/Pages.php
+++ app/Controllers/Pages.php
@@ -9,9 +9,7 @@ class Pages extends BaseController
         $data = [
             'title' => 'Home | WebProgrammingUNPAS'
         ];
-        echo view('layout/header', $data);
-        echo view('pages/home');
-        echo view('layout/footer');
+        return view('pages/home', $data);
     }
 
     public function about()
@@ -19,11 +17,28 @@ class Pages extends BaseController
         $data = [
             'title' => 'About | WebProgrammingUNPAS'
         ];
-        echo view('layout/header', $data);
-        echo view('pages/about');
-        echo view('layout/footer');
+        return view('pages/about', $data);
     }
 
+    public function contact()
+    {
+        $data = [
+            'title' => 'Contact Us',
+            'alamat' => [
+                [
+                    'tipe' => 'Rumah',
+                    'alamat' => 'Jl. abc No. 123',
+                    'kota' => 'Bandung'
+                ],
+                [
+                    'tipe' => 'Kantor',
+                    'alamat' => 'Jl. Setiabudi No. 192',
+                    'kota' => "Bandung"
+                ]
+            ]
+        ];
+        return view('pages/contact', $data);
+    }
     //--------------------------------------------------------------------
 
 }
