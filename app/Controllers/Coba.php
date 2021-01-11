a95b02416256182dee89d14d35ba33a5a6fd4adf
Dwi Fitriyanto <dwifitriyanto123@gmail.com>
2021-01-11 14:59:55 +0700

    first commit + routes & controller learning

---
+++ app/Controllers/Coba.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controllers;
+
+class Coba extends BaseController
+{
+    public function index()
+    {
+        echo "ini controller coba method index";
+    }
+
+    public function about($nama = '')
+    {
+        echo "Hallo, nama saya $nama";
+    }
+    //--------------------------------------------------------------------
+
+}
