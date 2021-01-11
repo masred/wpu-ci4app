1f337a8cef4aecb8acd9f3866335710fcf23c92e
Dwi Fitriyanto <dwifitriyanto123@gmail.com>
2021-01-11 18:18:59 +0700

    create layout

---
+++ app/Views/pages/about.php
@@ -0,0 +1,8 @@
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h1>About Me!</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam fugiat aperiam necessitatibus omnis aut libero similique aliquam, dolorem quod saepe! Facilis, voluptatibus atque esse nihil laboriosam repellat recusandae commodi at perferendis. Optio aut hic alias consequuntur voluptate eveniet ducimus est dicta doloribus distinctio maxime, itaque modi adipisci. In dolore tenetur dolorem doloremque eius commodi ipsa, quam ducimus quisquam incidunt aliquid iste tempora molestias consectetur nesciunt accusantium obcaecati et eveniet nobis vitae est dolorum eum aspernatur! Exercitationem eaque voluptatum consectetur fuga vero alias porro repellat, beatae tempora quam molestiae minima perspiciatis similique totam explicabo distinctio. Illum beatae inventore soluta quia reprehenderit?</p>
+        </div>
+    </div>
+</div>
