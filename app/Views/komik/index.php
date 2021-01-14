cb9efa79827f5f095e07b3a32fe789da394a7480
Dwi Fitriyanto <dwifitriyanto123@gmail.com>
2021-01-14 08:52:55 +0700

    added link detail

--- app/Views/komik/index.php
+++ app/Views/komik/index.php
@@ -21,7 +21,7 @@
                             <th scope="row"><?= $i++; ?></th>
                             <td><img src="/img/<?= $k['sampul']; ?>" class="sampul" alt=""></td>
                             <td><?= $k['judul']; ?></td>
-                            <td><a href="" class="btn btn-success">Detail</a></td>
+                            <td><a href="/komik/<?= $k['slug']; ?>" class="btn btn-success">Detail</a></td>
                         </tr>
                     <?php endforeach; ?>
                 </tbody>
